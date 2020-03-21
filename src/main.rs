d20deaede60c0bd24179a25b767fb8c3248efda8
fulstaph <bokovikov.g>
2020-03-21 03:04:18 +0300

    implemented multiplication

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,25 @@ impl Expression for Sum {
     }
 }
 
+struct Mul {
+    pub left: Box<dyn Expression>,
+    pub right: Box<dyn Expression>
+}
+
+
+impl Expression for Mul {
+    fn evaluate(&self) -> i32 {
+        self.left.evaluate() * self.right.evaluate()
+    }
+}
+
 fn main() {
-    let e = Sum{ left: Box::new(Number{ val: 2}),
-        right: Box::new(Number{ val: 2})
-    };
+    let e1 = Sum{left: Box::new(Number{val: 3}),
+                right: Box::new(Sum{ left: Box::new(Number{ val: 2}),
+                            right: Box::new(Number{ val: 2})
+    })};
+    let e2 = Sum{left: Box::new(Number{val:1}), right: Box::new(Number{val:2})};
+    let e = Mul{left: Box::new(e1), right: Box::new(e2)};
     let sum = (2 + 2);
     println!("default sum: {:#?}\neval sum: {:#?}", sum, e.evaluate());
 }
